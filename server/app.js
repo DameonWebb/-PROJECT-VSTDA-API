076e214b14cf698993c6a1dff0707d1ef6ae90e0
DameonWebb <dameonlwebb@gmail.com>
2022-05-24 11:46:43 -0700

    I have my server listening

--- server/app.js
+++ server/app.js
@@ -2,7 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
+app.listen(3000, (req, res) => {
+	console.log('listening on port 3000...')
+})
 // add your code here
+let arr = [
+	{
+		todoItemId: 0,
+		name: 'an item',
+		priority: 3,
+		completed: false
+	},
+	{
+		todoItemId: 1,
+		name: 'another item',
+		priority: 2,
+		completed: false
+	},
+	{
+		todoItemId: 2,
+		name: 'a done item',
+		priority: 1,
+		completed: true
+	}
+]; 
+
+app.get('/api/TodoItems', (req, res) => {
+   res.arr.send(todoItemId);
+}); 
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+const app = require('./app');
 const server = require('./app');
 
 // write your code here
+const port = 3000;
+app.get('/', (req, res) => {
+   res.status(200).send("ok!");
+}); 
